ec8f62dd14b329132d0dcaa1a0e10d62d07f178c
Jonathan Peter <jonathanpvdb@gmail.com>
2020-08-26 15:47:26 +0200

    test

--- src/app/components/config/skillsAndQs/configskills-and-qs/configskills-and-qs.component.ts
+++ src/app/components/config/skillsAndQs/configskills-and-qs/configskills-and-qs.component.ts
@@ -3,6 +3,9 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { EditAddSkillComponent } from '../../modals/edit-add-skill/edit-add-skill.component';
 import { EditAddQuestionComponent } from '../../modals/edit-add-question/edit-add-question.component';
 import { EditAddRequirementComponent } from '../../modals/edit-add-requirement/edit-add-requirement.component';
+import { PendingSkillComponent } from '../../modals/pending-skill/pending-skill.component';
+import { PendingQuestionComponent } from '../../modals/pending-question/pending-question.component';
+import { PendingRequirementComponent } from '../../modals/pending-requirement/pending-requirement.component';
 
 @Component({
   selector: 'app-configskills-and-qs',
@@ -25,5 +28,16 @@ export class ConfigskillsAndQsComponent implements OnInit {
   }
   addRequirement(){
     const modalIntance = this.modal.open(EditAddRequirementComponent);
+  }
+  pendingSkills(){
+    const modalInstance = this.modal.open(PendingSkillComponent, {windowClass : "largeModal"});
+  }
+
+  pendingQuestions(){
+    const modalInstance = this.modal.open(PendingQuestionComponent, {windowClass : "largeModal"});
+  }
+
+  pendingRequirements(){
+    const modalInstance = this.modal.open(PendingRequirementComponent, {windowClass : "largeModal"});
   }
 }
