b9d46e6b275129ccfec82abfbc0934c8bb644534
unknown <u16278055@tuks.co.za>
2020-09-10 23:21:19 +0200

    New Commit

--- src/app/components/forgotPassword/forgot-password/forgot-password.component.ts
+++ src/app/components/forgotPassword/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  userProfile :any;
+  userDetails : User;
+
+
+
+  constructor(private formBuilder : FormBuilder ) { }
 
   ngOnInit(): void {
+    this.buildForm();
+
   }
+  buildForm(){
+    this.userProfile = this.formBuilder.group({
+       //SHOULD I KEEP THE TEN DIGIT LIMIT OR LEAVE IT ?
+      email : ['',[Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9._]+\.[a-z]{1,4}")]],
+
+    });
+
+  }
+  getFormInformation(){
+
+    return{
+
+    email : this.userProfile.get('email').value
+
+    }
+  }
+
+  get userEmail(){
+    return this.userProfile.controls['email'];
+  }
+
+
+
+
+
+
 
 }
